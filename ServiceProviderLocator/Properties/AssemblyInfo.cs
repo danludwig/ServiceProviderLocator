8a45c580d743653cef1b775b9e37e83503ab2299
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-07-17 12:13:21 -0400

    Add class library project.

---
+++ ServiceProviderLocator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServiceProviderLocator")]
+[assembly: AssemblyDescription("Simple class for holding a static reference to an IServiceProvider instance, similar to System.Web.DependencyResolver.")]
+[assembly: AssemblyCompany("Dan Ludwig")]
+[assembly: AssemblyProduct("ServiceProviderLocator")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33497cb9-2713-4fe3-a64b-6acba6654ee9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
